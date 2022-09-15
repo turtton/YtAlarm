9f58d122f9912186356a2ab4445d239b2bcba754
turtton <top.gear7509@turtton.net>
2022-09-15 12:48:56 +0900

    build: fix typo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.room:room-testing:$${room?.version}")
+    androidTestImplementation("androidx.room:room-testing:${room?.version}")
 }
 
 kotlinter {
