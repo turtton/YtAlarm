6b3a8f85b011b04629d14837b99d830808b55c91
turtton <top.gear7509@turtton.net>
2022-09-15 11:15:19 +0900

    build: write dependencies version directly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,9 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp") version "1.7.10-1.0.6"
     id("androidx.navigation.safeargs.kotlin")
-    kotlin("plugin.serialization") version Versions.KOTLIN
+    kotlin("plugin.serialization") version "1.7.10"
 }
+
 
 android {
     compileSdk = 32
@@ -55,31 +56,31 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:${Versions.CORE}")
-    implementation("androidx.appcompat:appcompat:${Versions.APP_COMPAT}")
-    implementation("com.google.android.material:material:${Versions.MATERIAL}")
-    implementation("androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINT_LAYOUT}")
-    implementation("androidx.navigation:navigation-fragment-ktx:${Versions.NAVIGATION}")
-    implementation("androidx.navigation:navigation-ui-ktx:${Versions.NAVIGATION}")
-    implementation("androidx.recyclerview:recyclerview:${Versions.RECYCLER_VIEW}")
-    implementation("com.android.support:support-annotations:${Versions.ANNOTATIONS}")
-    implementation("androidx.room:room-ktx:${Versions.ROOM}")
-    ksp("androidx.room:room-compiler:${Versions.ROOM}")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.LIFECYCLE}")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:${Versions.LIFECYCLE}")
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("com.android.support:support-annotations:28.0.0")
+    val room = implementation("androidx.room:room-ktx:2.4.3")
+    ksp("androidx.room:room-compiler:${room?.version}")
+    val lifecycle = implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:${lifecycle?.version}")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.COROUTINE}")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.SERIALIZATION}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
 
-    implementation("com.github.yausername.youtubedl-android:library:${Versions.YT_DL}")
-    implementation("com.github.yausername.youtubedl-android:common:${Versions.YT_DL}")
-    implementation("com.github.bumptech.glide:glide:${Versions.GLIDE}")
-    annotationProcessor("com.github.bumptech.glide:compiler:${Versions.GLIDE}")
-    implementation("com.michael-bull.kotlin-result:kotlin-result:${Versions.KOTLIN_RESULT}")
-    implementation("com.michael-bull.kotlin-result:kotlin-result-coroutines:${Versions.KOTLIN_RESULT}")
+    val ytdl = implementation("com.github.yausername.youtubedl-android:library:a8a636e6b3")
+    implementation("com.github.yausername.youtubedl-android:common:${ytdl?.version}")
+    val glide = implementation("com.github.bumptech.glide:glide:4.13.2")
+    annotationProcessor("com.github.bumptech.glide:compiler:${glide?.version}")
+    val kotlinResult = implementation("com.michael-bull.kotlin-result:kotlin-result:1.1.16")
+    implementation("com.michael-bull.kotlin-result:kotlin-result-coroutines:${kotlinResult?.version}")
 
-    testImplementation("junit:junit:${Versions.JUNIT}")
-    androidTestImplementation("androidx.test.ext:junit:${Versions.JUNIT_ANDROID}")
-    androidTestImplementation("androidx.test.espresso:espresso-core:${Versions.ESPRESSO}")
-    androidTestImplementation("androidx.room:room-testing:${Versions.ROOM}")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.room:room-testing:$${room?.version}")
 }
