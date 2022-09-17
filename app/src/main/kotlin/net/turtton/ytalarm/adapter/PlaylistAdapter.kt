2a8cbfcbc8aae4dec55520420482028b6bab3889
turtton <top.gear7509@turtton.net>
2022-09-18 00:15:53 +0900

    feat: implement SelectionTracker and menu

--- app/src/main/kotlin/net/turtton/ytalarm/adapter/PlaylistAdapter.kt
+++ app/src/main/kotlin/net/turtton/ytalarm/adapter/PlaylistAdapter.kt
@@ -1,11 +1,17 @@
 package net.turtton.ytalarm.adapter
 
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.findNavController
+import androidx.recyclerview.selection.ItemDetailsLookup
+import androidx.recyclerview.selection.ItemDetailsLookup.ItemDetails
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.SelectionTracker.SelectionObserver
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -17,27 +23,37 @@ import net.turtton.ytalarm.util.BasicComparator
 class PlaylistAdapter : ListAdapter<Playlist, PlaylistAdapter.ViewHolder>(
     BasicComparator<Playlist>()
 ) {
+    private val currentCheckBox = hashSetOf<Pair<Long, CheckBox>>()
+
+    var tracker: SelectionTracker<Long>? = null
+        set(value) {
+            value?.let {
+                it.addObserver(object : SelectionObserver<Long>() {
+                    override fun onSelectionChanged() {
+                        currentCheckBox.forEach { (id, box) ->
+                            box.visibility = if (it.hasSelection()) {
+                                View.VISIBLE
+                            } else {
+                                View.GONE
+                            }
+                            box.isChecked = it.isSelected(id)
+                        }
+                    }
+                })
+            }
+            field = value
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        @Suppress("ktlint:argument-list-wrapping")
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_playlist, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindData(getItem(position))
-    }
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        lateinit var playlist: Playlist
-
-        private val title: TextView = view.findViewById(R.id.item_playlist_title)
-        private val videoCount: TextView = view.findViewById(R.id.item_playlist_video_count)
-        private val thumbnail: ImageView = view.findViewById(R.id.item_playlist_thumbnail)
-
-        fun bindData(data: Playlist) {
-            playlist = data
+        val data = getItem(position)
+        holder.itemView.tag = data.id.toLong()
+        holder.apply {
             title.text = data.title
             val size = data.videos.size
             if (data.videos.isNotEmpty()) {
@@ -52,12 +68,56 @@ class PlaylistAdapter : ListAdapter<Playlist, PlaylistAdapter.ViewHolder>(
                 )
             }
             Glide.with(itemView).load(data.thumbnailUrl).into(thumbnail)
+            tracker?.let {
+                val isSelected = it.isSelected(data.id.toLong())
+                itemView.isActivated = isSelected
+                checkBox.isChecked = isSelected
+                checkBox.visibility = if (it.hasSelection()) {
+                    View.VISIBLE
+                } else {
+                    View.GONE
+                }
+            }
 
             itemView.setOnClickListener {
                 val action = FragmentPlaylistDirections.actionPlaylistFragmentToVideoListFragment(
                     data.id
                 )
                 itemView.findNavController().navigate(action)
+            }
+        }
+    }
+
+    override fun onViewDetachedFromWindow(holder: ViewHolder) {
+        currentCheckBox.remove(holder.itemView.tag as Long to holder.checkBox)
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(R.id.item_playlist_title)
+        val videoCount: TextView = view.findViewById(R.id.item_playlist_video_count)
+        val thumbnail: ImageView = view.findViewById(R.id.item_playlist_thumbnail)
+        val checkBox: CheckBox = view.findViewById(R.id.item_playlist_checkbox)
+
+        init {
+            checkBox.visibility = View.GONE
+        }
+
+        fun toItemDetail(): ItemDetails<Long> =
+            object : ItemDetails<Long>() {
+                override fun getPosition(): Int = absoluteAdapterPosition
+                override fun getSelectionKey(): Long? = itemView.tag as Long?
+            }
+    }
+
+    class PlaylistDetailsLookup(val recyclerView: RecyclerView) : ItemDetailsLookup<Long>() {
+        override fun getItemDetails(e: MotionEvent): ItemDetails<Long>? {
+            return recyclerView.findChildViewUnder(e.x, e.y)?.let {
+                val viewHolder = recyclerView.getChildViewHolder(it)
+                if (viewHolder is ViewHolder) {
+                    viewHolder.toItemDetail()
+                } else {
+                    null
+                }
             }
         }
     }
--- app/src/main/kotlin/net/turtton/ytalarm/fragment/FragmentPlaylist.kt
+++ app/src/main/kotlin/net/turtton/ytalarm/fragment/FragmentPlaylist.kt
@@ -3,23 +3,51 @@ package net.turtton.ytalarm.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StorageStrategy
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 import net.turtton.ytalarm.MainActivity
+import net.turtton.ytalarm.R
 import net.turtton.ytalarm.YtApplication.Companion.repository
 import net.turtton.ytalarm.adapter.PlaylistAdapter
+import net.turtton.ytalarm.fragment.dialog.DialogRemoveVideo
+import net.turtton.ytalarm.util.AttachableMenuProvider
+import net.turtton.ytalarm.util.SelectionMenuObserver
+import net.turtton.ytalarm.util.SelectionTrackerContainer
+import net.turtton.ytalarm.util.TagKeyProvider
 import net.turtton.ytalarm.viewmodel.PlaylistViewModel
 import net.turtton.ytalarm.viewmodel.PlaylistViewModelFactory
 
-class FragmentPlaylist : FragmentAbstractList() {
+class FragmentPlaylist : FragmentAbstractList(), SelectionTrackerContainer<Long> {
     private val playlistViewModel: PlaylistViewModel by viewModels {
         PlaylistViewModelFactory(requireActivity().application.repository)
     }
 
+    override lateinit var selectionTracker: SelectionTracker<Long>
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.recyclerList.layoutManager = LinearLayoutManager(view.context)
+        val recyclerView = binding.recyclerList
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
         val adapter = PlaylistAdapter()
-        binding.recyclerList.adapter = adapter
+        recyclerView.adapter = adapter
+
+        selectionTracker = SelectionTracker.Builder(
+            "PlaylistTracker",
+            recyclerView,
+            TagKeyProvider(recyclerView),
+            PlaylistAdapter.PlaylistDetailsLookup(recyclerView),
+            StorageStrategy.createLongStorage()
+        ).build()
+        adapter.tracker = selectionTracker
+
+        selectionTracker.addObserver(PlaylistSelectionObserver(this))
+
+        savedInstanceState?.let {
+            selectionTracker.onRestoreInstanceState(it)
+        }
 
         playlistViewModel.allPlaylists.observe(requireActivity()) {
             it?.let { adapter.submitList(it) }
@@ -34,4 +62,30 @@ class FragmentPlaylist : FragmentAbstractList() {
             // TODO("open playlist name input dialog and create new Playlist in this fragment")
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        selectionTracker.onSaveInstanceState(outState)
+    }
+
+    class PlaylistSelectionObserver(
+        fragment: FragmentPlaylist
+    ) : SelectionMenuObserver<Long, FragmentPlaylist>(
+        fragment,
+        AttachableMenuProvider(
+            fragment,
+            R.menu.menu_playlist_action,
+            R.id.menu_playlist_action_remove to {
+                val selection = fragment.selectionTracker.selection.map { it.toInt() }
+                DialogRemoveVideo { _, _ ->
+                    val async = fragment.playlistViewModel.getFromIdsAsync(selection)
+                    fragment.lifecycleScope.launch {
+                        val playlists = async.await()
+                        fragment.playlistViewModel.delete(playlists)
+                    }
+                    fragment.selectionTracker.clearSelection()
+                }.show(fragment.childFragmentManager, "PlaylistRemoveDialog")
+                true
+            }
+        )
+    )
 }
