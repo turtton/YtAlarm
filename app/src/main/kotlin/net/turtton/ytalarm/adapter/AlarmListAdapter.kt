d57371018c7544da6ed4b0287672165ddf725875
turtton <top.gear7509@turtton.net>
2022-09-18 22:07:37 +0900

    feat: add alarm registration

--- app/src/main/kotlin/net/turtton/ytalarm/adapter/AlarmListAdapter.kt
+++ app/src/main/kotlin/net/turtton/ytalarm/adapter/AlarmListAdapter.kt
@@ -1,5 +1,9 @@
 package net.turtton.ytalarm.adapter
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,11 +17,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import net.turtton.ytalarm.AlarmActivity
 import net.turtton.ytalarm.R
 import net.turtton.ytalarm.fragment.FragmentAlarmList
 import net.turtton.ytalarm.fragment.FragmentAlarmListDirections
 import net.turtton.ytalarm.structure.Alarm
 import net.turtton.ytalarm.util.BasicComparator
+import java.util.Calendar
 
 class AlarmListAdapter(
     private val parentFragment: FragmentAlarmList
@@ -31,8 +37,32 @@ class AlarmListAdapter(
         val data = getItem(position)
         holder.apply {
             alarmTime.text = data.time
-            alarmType.text = data.repeatType.getDisplay(itemView.context)
+            val context = itemView.context
+            alarmType.text = data.repeatType.getDisplay(context)
             alarmSwitch.isChecked = data.enable
+            alarmSwitch.setOnCheckedChangeListener { _, isChecked ->
+                val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
+                if (alarmManager != null) {
+                    val intent = Intent(context, AlarmActivity::class.java)
+                    intent.putExtra(AlarmActivity.EXTRA_ALARM_ID, data.id!!)
+                    val pendingIntent = PendingIntent.getActivity(
+                        context,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_IMMUTABLE
+                    )
+                    val calendar = Calendar.getInstance()
+                    val (hour, minute) = data.time.split(':').map { it.toInt() }
+                    calendar.set(Calendar.HOUR_OF_DAY, hour)
+                    calendar.set(Calendar.MINUTE, minute)
+                    val clockInfo = AlarmManager.AlarmClockInfo(calendar.timeInMillis, null)
+                    if (isChecked) {
+                        alarmManager.setAlarmClock(clockInfo, pendingIntent)
+                    } else {
+                        alarmManager.cancel(pendingIntent)
+                    }
+                }
+            }
 
             val async = parentFragment.playlistViewModel.getFromIdAsync(data.playListId!!)
             parentFragment.lifecycleScope.launch {
