152aef176e0dc6cf790b2aa6963b3542d58902ed
turtton <top.gear7509@turtton.net>
2022-09-18 11:12:11 +0900

    fix: change passing data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.5.0")
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")?.also {
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")?.version?.also {
         implementation("androidx.navigation:navigation-ui-ktx:$it")
     }
     implementation("androidx.recyclerview:recyclerview:1.2.1")
