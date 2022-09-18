4207afec91c10a077e10516dc1594565702e62b0
turtton <top.gear7509@turtton.net>
2022-09-18 20:53:00 +0900

    refactor: change playlist import process

--- app/src/main/kotlin/net/turtton/ytalarm/adapter/AlarmListAdapter.kt
+++ app/src/main/kotlin/net/turtton/ytalarm/adapter/AlarmListAdapter.kt
@@ -6,10 +6,13 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.SwitchCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import net.turtton.ytalarm.R
 import net.turtton.ytalarm.fragment.FragmentAlarmList
 import net.turtton.ytalarm.fragment.FragmentAlarmListDirections
@@ -30,19 +33,17 @@ class AlarmListAdapter(
             aramTime.text = data.time
             aramType.text = data.repeatType.getDisplay(itemView.context)
             aramSwitch.isChecked = data.enable
-            parentFragment.playlistViewModel
-                .getFromId(data.playListId!!)
-                .observe(parentFragment.requireActivity()) { playlist ->
-                    playlist?.also {
+            val async = parentFragment.playlistViewModel.getFromIdAsync(data.playListId!!)
+            parentFragment.lifecycleScope.launch {
+                async.await()?.let {
+                    launch(Dispatchers.Main) {
                         playlistName.text = it.title
-                        it.thumbnailUrl?.also { thumbnail ->
-                            Glide.with(itemView).load(thumbnail).into(aramThumbnail)
-                        }
+                        Glide.with(itemView).load(it.thumbnailUrl).into(aramThumbnail)
                     }
                 }
+            }
 
             itemView.setOnClickListener {
-                @Suppress("ktlint:argument-list-wrapping")
                 val action = FragmentAlarmListDirections
                     .actionAlarmListFragmentToAlarmSettingsFragment(data.id!!)
                 parentFragment.findNavController().navigate(action)
