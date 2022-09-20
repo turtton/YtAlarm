8e42d3b4419bf2f927a2957737b6612399ab5dd2
turtton <top.gear7509@turtton.net>
2022-09-20 16:08:51 +0900

    feat: add DayOfWeekCompat extensions

---
+++ app/src/main/kotlin/net/turtton/ytalarm/util/extensions/DayOfWeekCompat.kt
@@ -0,0 +1,33 @@
+package net.turtton.ytalarm.util.extensions
+
+import net.turtton.ytalarm.util.DayOfWeekCompat
+
+fun List<DayOfWeekCompat>.getNearestWeekOrNull(calendarDayOfWeek: Int): DayOfWeekCompat? {
+    var nearestWeek: DayOfWeekCompat? = null
+    var nearestWeekDiff: Int? = null
+
+    var nextWeek: DayOfWeekCompat? = null
+    var nextWeekDiff: Int? = null
+
+    associateWith(DayOfWeekCompat::convertCalenderCode)
+        .forEach { (dayOfWeek, calendarCode) ->
+            val diff = calendarCode - calendarDayOfWeek
+            if (diff < 0) {
+                if (nextWeek == null || nextWeekDiff!! > diff) {
+                    nextWeek = dayOfWeek
+                    nextWeekDiff = diff
+                }
+            } else {
+                if (nearestWeek == null || nearestWeekDiff!! > diff) {
+                    nearestWeek = dayOfWeek
+                    nearestWeekDiff = diff
+                }
+            }
+        }
+
+    return nearestWeek ?: nextWeek
+}
+
+fun List<DayOfWeekCompat>.getNearestWeek(calendarDayOfWeek: Int): DayOfWeekCompat {
+    return getNearestWeekOrNull(calendarDayOfWeek) ?: throw IllegalStateException("Empty list")
+}
---
+++ app/src/test/kotlin/net/turtton/ytalarm/util/extensions/TestDayOfWeekCompat.kt
@@ -0,0 +1,35 @@
+package net.turtton.ytalarm.util.extensions
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import net.turtton.ytalarm.util.DayOfWeekCompat
+import java.util.Calendar
+
+@Suppress("UNUSED")
+class TestDayOfWeekCompat : FunSpec({
+    context("getNearestWeekOrNull") {
+        test("contains same day") {
+            listOf(
+                DayOfWeekCompat.SUNDAY,
+                DayOfWeekCompat.WEDNESDAY,
+                DayOfWeekCompat.FRIDAY
+            ).getNearestWeekOrNull(Calendar.WEDNESDAY) shouldBe DayOfWeekCompat.WEDNESDAY
+        }
+
+        test("same week") {
+            listOf(
+                DayOfWeekCompat.WEDNESDAY,
+                DayOfWeekCompat.FRIDAY,
+                DayOfWeekCompat.SATURDAY
+            ).getNearestWeekOrNull(Calendar.SUNDAY) shouldBe DayOfWeekCompat.WEDNESDAY
+        }
+
+        test("next week") {
+            listOf(
+                DayOfWeekCompat.SUNDAY,
+                DayOfWeekCompat.WEDNESDAY,
+                DayOfWeekCompat.THURSDAY
+            ).getNearestWeekOrNull(Calendar.SATURDAY) shouldBe DayOfWeekCompat.SUNDAY
+        }
+    }
+})
