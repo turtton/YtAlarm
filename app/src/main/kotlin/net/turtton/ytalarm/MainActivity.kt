27d0fa17bad7c2a47e8bc0cd2cb1864dfff8d8e7
turtton <top.gear7509@turtton.net>
2022-10-07 18:05:08 +0900

    feat: add dialog to disable option "remove permission if app unused"

--- app/src/main/kotlin/net/turtton/ytalarm/MainActivity.kt
+++ app/src/main/kotlin/net/turtton/ytalarm/MainActivity.kt
@@ -10,6 +10,10 @@ import android.provider.Settings
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.IntentCompat
+import androidx.core.content.PackageManagerCompat
+import androidx.core.content.UnusedAppRestrictionsConstants
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
@@ -18,6 +22,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import net.turtton.ytalarm.databinding.ActivityMainBinding
 import net.turtton.ytalarm.util.SNOOZE_NOTIFICATION
 import net.turtton.ytalarm.util.initYtDL
@@ -73,6 +78,35 @@ class MainActivity : AppCompatActivity() {
                 .setPositiveButton(R.string.dialog_require_overlay_perm_ok) { _, _ ->
                     activity.launch(intent)
                 }.show()
+        } else {
+            PackageManagerCompat.getUnusedAppRestrictionsStatus(this).let {
+                it.addListener({
+                    when (it.get()) {
+                        UnusedAppRestrictionsConstants.API_30_BACKPORT,
+                        UnusedAppRestrictionsConstants.API_30,
+                        UnusedAppRestrictionsConstants.API_31 -> {
+                            val intent = IntentCompat
+                                .createManageUnusedAppRestrictionsIntent(this, packageName)
+                            AlertDialog.Builder(this)
+                                .setTitle(R.string.dialog_require_configuration)
+                                .setMessage(R.string.dialog_require_disable_restrictions)
+                                .setPositiveButton(
+                                    R.string.dialog_require_disable_restrictions_ok
+                                ) { _, _ ->
+                                    activity.launch(intent)
+                                }.show()
+                        }
+                        UnusedAppRestrictionsConstants.ERROR -> {
+                            Snackbar.make(
+                                binding.root.rootView,
+                                R.string.snackbar_failed_to_check_restriction,
+                                900
+                            )
+                                .show()
+                        }
+                    }
+                }, ContextCompat.getMainExecutor(this))
+            }
         }
     }
 
