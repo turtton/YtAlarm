d5609671473583dd7f7ff9e8cf523fa391b62b84
turtton <top.gear7509@turtton.net>
2022-10-14 17:34:56 +0900

    fix: improve VideoInformation type safety

---
+++ app/src/main/kotlin/net/turtton/ytalarm/util/serializer/VideoInformationSerializer.kt
@@ -0,0 +1,62 @@
+package net.turtton.ytalarm.util.serializer
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import net.turtton.ytalarm.util.VideoInformation
+
+object VideoInformationSerializer : KSerializer<VideoInformation> {
+    override val descriptor = VideoInformationSurrogate.serializer().descriptor
+
+    override fun deserialize(decoder: Decoder): VideoInformation {
+        val surrogate = decoder.decodeSerializableValue(VideoInformationSurrogate.serializer())
+        val type = when (surrogate.type) {
+            "video" -> VideoInformation.Type.Video(
+                surrogate.fullTitle!!,
+                surrogate.thumbnailUrl!!,
+                surrogate.videoUrl!!
+            )
+            "playlist" -> VideoInformation.Type.Playlist(
+                surrogate.entries!!
+            )
+            else -> error("Unknown type name ${surrogate.type}")
+        }
+        return VideoInformation(
+            surrogate.id,
+            surrogate.title,
+            surrogate.url,
+            surrogate.domain,
+            type
+        )
+    }
+
+    override fun serialize(encoder: Encoder, value: VideoInformation) {
+        error("Not implemented")
+    }
+
+    @Serializable
+    private class VideoInformationSurrogate(
+        val id: String,
+        val title: String? = null,
+        @SerialName("webpage_url")
+        val url: String,
+        @SerialName("webpage_url_domain")
+        val domain: String,
+        // video or playlist
+        @SerialName("_type")
+        val type: String = "video",
+        /* available if type == video */
+        @SerialName("fulltitle")
+        val fullTitle: String? = null,
+        @SerialName("thumbnail")
+        val thumbnailUrl: String? = null,
+        @SerialName("url")
+        val videoUrl: String? = null,
+        /* end */
+        /* available if type == playlist */
+        val entries: List<VideoInformation>? = null
+        /* end */
+    )
+}
---
+++ app/src/test/kotlin/net/turtton/ytalarm/util/serializer/TestVideoInformationSerializer.kt
@@ -0,0 +1,63 @@
+package net.turtton.ytalarm.util.serializer
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import net.turtton.ytalarm.util.VideoInformation
+
+@Suppress("UNUSED")
+class TestVideoInformationSerializer : FunSpec({
+    val json = Json { ignoreUnknownKeys = true }
+    context("decode") {
+        val videoData = VideoInformation(
+            "abc",
+            "title",
+            "example.com/abc",
+            "example.com",
+            VideoInformation.Type.Video(
+                "fullTitle",
+                "thumbnail.example.com/abc",
+                "video.example.com/abc"
+            )
+        )
+        val rowVideoData = """
+            {
+                "id": "abc",
+                "title": "title",
+                "webpage_url": "example.com/abc",
+                "webpage_url_domain": "example.com",
+                "_type": "video",
+                "fulltitle": "fullTitle",
+                "thumbnail": "thumbnail.example.com/abc",
+                "url": "video.example.com/abc"
+            }
+        """.trimIndent()
+        test("video") {
+            val decoded = json.decodeFromString<VideoInformation>(rowVideoData)
+            decoded shouldBe videoData
+        }
+        test("playlist") {
+            val exampleData = """
+                {
+                    "id": "abc",
+                    "title": "title",
+                    "webpage_url": "example.com/abc",
+                    "webpage_url_domain": "example.com",
+                    "_type": "playlist",
+                    "entries": [$rowVideoData]
+                }
+            """.trimIndent()
+            val expected = VideoInformation(
+                "abc",
+                "title",
+                "example.com/abc",
+                "example.com",
+                VideoInformation.Type.Playlist(listOf(videoData))
+            )
+
+            val decoded = json.decodeFromString<VideoInformation>(exampleData)
+            decoded shouldBe expected
+        }
+    }
+})
