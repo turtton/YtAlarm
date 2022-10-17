931e853d845ced54b5871030d9b193cf988d52bd
turtton <top.gear7509@turtton.net>
2022-10-17 23:40:31 +0900

    feat: set workManager logging level

--- app/src/main/kotlin/net/turtton/ytalarm/YtApplication.kt
+++ app/src/main/kotlin/net/turtton/ytalarm/YtApplication.kt
@@ -1,14 +1,20 @@
 package net.turtton.ytalarm
 
 import android.app.Application
+import android.util.Log
+import androidx.work.Configuration
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import net.turtton.ytalarm.database.AppDatabase
 
-class YtApplication : Application() {
+class YtApplication : Application(), Configuration.Provider {
     val appCoroutineScope = CoroutineScope(SupervisorJob())
     val database by lazy { AppDatabase.getDataBase(this, appCoroutineScope) }
     val repository by lazy { DataRepository(database) }
+
+    override fun getWorkManagerConfiguration(): Configuration = Configuration.Builder()
+        .setMinimumLoggingLevel(if (BuildConfig.DEBUG) Log.DEBUG else Log.ERROR)
+        .build()
 
     companion object {
         val Application.repository: DataRepository
