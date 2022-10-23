5f4de4dbffe4d62780e6cc41ae65e3d1b2cf3304
turtton <top.gear7509@turtton.net>
2022-10-23 14:57:06 +0900

    fix: SelectionTracker may throw an exception

--- app/src/main/kotlin/net/turtton/ytalarm/ui/selection/SelectionTrackerContainer.kt
+++ app/src/main/kotlin/net/turtton/ytalarm/ui/selection/SelectionTrackerContainer.kt
@@ -3,5 +3,5 @@ package net.turtton.ytalarm.ui.selection
 import androidx.recyclerview.selection.SelectionTracker
 
 interface SelectionTrackerContainer<T> {
-    val selectionTracker: SelectionTracker<T>
+    val selectionTracker: SelectionTracker<T>?
 }
