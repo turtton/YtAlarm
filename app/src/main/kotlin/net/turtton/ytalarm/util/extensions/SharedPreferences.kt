1e2e4c2e8d361d175c697f5cc4275aaf3795dc4c
turtton <top.gear7509@turtton.net>
2022-10-24 19:16:32 +0900

    feat: add order option value getter

---
+++ app/src/main/kotlin/net/turtton/ytalarm/util/extensions/SharedPreferences.kt
@@ -0,0 +1,47 @@
+package net.turtton.ytalarm.util.extensions
+
+import android.content.SharedPreferences
+import android.view.View
+import androidx.core.content.edit
+import com.google.android.material.snackbar.Snackbar
+import net.turtton.ytalarm.R
+import net.turtton.ytalarm.util.order.AlarmOrder
+import net.turtton.ytalarm.util.order.PlaylistOrder
+import net.turtton.ytalarm.util.order.VideoOrder
+
+fun SharedPreferences?.showMessageIfNull(view: View): SharedPreferences? = also {
+    if (it == null) {
+        val message = R.string.snackbar_error_failed_to_access_settings_data
+        Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show()
+    }
+}
+
+private const val ALARM_ORDER = "AlarmOrder"
+var SharedPreferences.alarmOrderUp: Boolean
+    get() = getBoolean(ALARM_ORDER, true)
+    set(value) = edit { putBoolean(ALARM_ORDER, value) }
+
+private const val VIDEO_ORDER = "VideoOrder"
+var SharedPreferences.videoOrderUp: Boolean
+    get() = getBoolean(VIDEO_ORDER, true)
+    set(value) = edit { putBoolean(VIDEO_ORDER, value) }
+
+private const val PLAYLIST_ORDER = "PlaylistOrder"
+var SharedPreferences.playlistOrderUp: Boolean
+    get() = getBoolean(PLAYLIST_ORDER, true)
+    set(value) = edit { putBoolean(PLAYLIST_ORDER, value) }
+
+private const val ALARM_ORDER_RULE = "AlarmOrderRule"
+var SharedPreferences.alarmOrderRule: AlarmOrder
+    get() = AlarmOrder.valueOf(getString(ALARM_ORDER_RULE, AlarmOrder.TIME.name)!!)
+    set(value) = edit { putString(ALARM_ORDER_RULE, value.name) }
+
+private const val VIDEO_ORDER_RULE = "VideoOrderRule"
+var SharedPreferences.videoOrderRule: VideoOrder
+    get() = VideoOrder.valueOf(getString(VIDEO_ORDER_RULE, VideoOrder.TITLE.name)!!)
+    set(value) = edit { putString(VIDEO_ORDER_RULE, value.name) }
+
+private const val PLAYLIST_ORDER_RULE = "PlaylistOrderRule"
+var SharedPreferences.playlistOrderRule: PlaylistOrder
+    get() = PlaylistOrder.valueOf(getString(PLAYLIST_ORDER_RULE, PlaylistOrder.TITLE.name)!!)
+    set(value) = edit { putString(PLAYLIST_ORDER_RULE, value.name) }
