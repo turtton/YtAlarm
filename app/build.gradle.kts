d0fa3379f3a382ac23486ef6d5722d3fb18d4277
turtton <top.gear7509@turtton.net>
2024-02-04 11:08:40 +0900

    chore: Upgrade youtubedl-android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -144,7 +144,7 @@ dependencies {
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:$it")
     }
 
-    implementation("com.github.yausername.youtubedl-android:library:4f00394209")?.also {
+    implementation("com.github.yausername.youtubedl-android:library:ed652962e5")?.also {
         implementation("com.github.yausername.youtubedl-android:common:${it.version}")
     }
     implementation("com.github.bumptech.glide:glide:4.16.0")?.also {
