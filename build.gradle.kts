1b21e6d413b4d12f02e3ed57fc97fc937f1d7ee7
turtton <top.gear7509@turtton.net>
2024-02-04 15:13:41 +0900

    chore: Update to beta version

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,8 @@ buildscript {
     }
 }
 plugins {
-    id("com.android.application") version "8.3.0-alpha05" apply false
-    id("com.android.library") version "8.3.0-alpha05" apply false
+    id("com.android.application") version "8.3.0-beta02" apply false
+    id("com.android.library") version "8.3.0-beta02" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("org.jmailen.kotlinter") version "4.2.0" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.5" apply false
