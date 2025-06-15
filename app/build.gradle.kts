9c3c19cf26c2e53cd46d42924a07040ab8b64415
turtton <top.gear7509@turtton.net>
2025-06-15 14:55:20 +0900

    refactor: Use version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp") version "2.0.21-1.0.28"
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
     id("androidx.navigation.safeargs.kotlin")
-    kotlin("plugin.serialization") version "2.0.21"
-    id("org.jmailen.kotlinter")
-    id("io.gitlab.arturbosch.detekt")
-    id("androidx.room")
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlinter)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.androidx.room)
 }
 
 // This versioning probably follows semver.org
@@ -20,13 +20,13 @@ val patch = 1
 val hasNoSplits = hasProperty("noSplits")
 
 android {
-    compileSdk = 35
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "net.turtton.ytalarm"
         namespace = applicationId
-        minSdk = 24
-        targetSdk = 35
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
 
         val versionNamePatchVer = patch * 5
         val abiFilterList = property("abiFilters").toString().split(';')
@@ -119,63 +119,43 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")?.version?.also {
-        implementation("androidx.navigation:navigation-ui-ktx:$it")
-        androidTestImplementation("androidx.navigation:navigation-testing:$it")
-    }
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
-    implementation("com.android.support:support-annotations:28.0.0")
-    val room = implementation("androidx.room:room-ktx:2.7.1")
-    ksp("androidx.room:room-compiler:${room?.version}")
-    implementation("androidx.room:room-runtime:${room?.version}")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")?.also {
-        implementation("androidx.lifecycle:lifecycle-livedata-ktx:${it.version}")
-    }
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("androidx.work:work-runtime-ktx:2.9.0")?.version?.also {
-        androidTestImplementation("androidx.work:work-testing:$it")
-    }
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.bundles.androidx.navigation)
+    androidTestImplementation(libs.androidx.navigation.testing)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.recyclerview.selection)
+    implementation(libs.android.support.annotations)
+    implementation(libs.bundles.androidx.room)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.bundles.androidx.lifecycle)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.work.runtime.ktx)
+    androidTestImplementation(libs.androidx.work.testing)
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")?.version?.also {
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:$it")
-    }
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.bundles.kotlinx.serialization)
 
-    implementation("com.github.yausername.youtubedl-android:library:0.15.0")?.also {
-        implementation("com.github.yausername.youtubedl-android:common:${it.version}")
-    }
-    implementation("com.github.bumptech.glide:glide:4.16.0")?.also {
-        annotationProcessor("com.github.bumptech.glide:compiler:${it.version}")
-    }
-    implementation("io.arrow-kt:arrow-core:2.1.0")?.also {
-        implementation("io.arrow-kt:arrow-fx-coroutines:${it.version}")
-    }
+    implementation(libs.bundles.youtubedl)
+    implementation(libs.glide)
+    annotationProcessor(libs.glide.compiler)
+    implementation(libs.bundles.arrow)
 
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("io.kotest:kotest-runner-junit5:5.8.1")?.version?.also {
-        testImplementation("io.kotest:kotest-assertions-core:$it")?.also { dep ->
-            androidTestImplementation(dep)
-        }
-        testImplementation("io.kotest:kotest-property:$it")
-    }
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.3.1")
-    testImplementation("org.robolectric:robolectric:4.12.1")
-    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
-    androidTestImplementation("androidx.test:rules:1.5.0")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")?.version?.also {
-        implementation("androidx.test.espresso:espresso-idling-resource:$it")
-        androidTestImplementation("androidx.test.espresso:espresso-contrib:$it")
-    }
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:")
-    androidTestImplementation("tools.fastlane:screengrab:2.1.1")
-    androidTestImplementation("androidx.room:room-testing:${room?.version}")
+    testImplementation(libs.junit)
+    testImplementation(libs.bundles.kotest)
+    androidTestImplementation(libs.kotest.assertions.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.robolectric)
+    androidTestImplementation(libs.androidx.test.ext.junit.ktx)
+    androidTestImplementation(libs.androidx.test.rules)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.bundles.androidx.test.espresso)
+    implementation(libs.androidx.test.espresso.idling.resource)
+    androidTestImplementation(libs.screengrab)
+    androidTestImplementation(libs.androidx.room.testing)
 }
 
 detekt {
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,33 +4,33 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+        classpath(libs.androidx.navigation.safe.args)
     }
     configurations.classpath {
         resolutionStrategy {
             force(
-                "com.pinterest.ktlint:ktlint-rule-engine:1.2.1",
-                "com.pinterest.ktlint:ktlint-rule-engine-core:1.2.1",
-                "com.pinterest.ktlint:ktlint-cli-reporter-core:1.2.1",
-                "com.pinterest.ktlint:ktlint-cli-reporter-checkstyle:1.2.1",
-                "com.pinterest.ktlint:ktlint-cli-reporter-json:1.2.1",
-                "com.pinterest.ktlint:ktlint-cli-reporter-html:1.2.1",
-                "com.pinterest.ktlint:ktlint-cli-reporter-plain:1.2.1",
-                "com.pinterest.ktlint:ktlint-cli-reporter-sarif:1.2.1",
-                "com.pinterest.ktlint:ktlint-ruleset-standard:1.2.1"
+                libs.ktlint.rule.engine,
+                libs.ktlint.rule.engine.core,
+                libs.ktlint.cli.reporter.core,
+                libs.ktlint.cli.reporter.checkstyle,
+                libs.ktlint.cli.reporter.json,
+                libs.ktlint.cli.reporter.html,
+                libs.ktlint.cli.reporter.plain,
+                libs.ktlint.cli.reporter.sarif,
+                libs.ktlint.ruleset.standard
             )
         }
     }
 }
 plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("com.android.library") version "8.3.2" apply false
-    id("androidx.room") version "2.7.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jmailen.kotlinter") version "4.3.0" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.23.6" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.androidx.room) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlinter) apply false
+    alias(libs.plugins.detekt) apply false
     // Related: https://github.com/NeoTech-Software/Android-Root-Coverage-Plugin?tab=readme-ov-file#4-compatibility
-    id("nl.neotech.plugin.rootcoverage") version "1.8.0"
+    alias(libs.plugins.root.coverage)
 }
 
 tasks.create("clean") {
