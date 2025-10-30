43b85b8e448b9a029ee309665735ff9704fd20db
turtton <top.gear7509@turtton.net>
2025-10-30 22:19:30 +0900

    feat: Add custom color/theme

---
+++ app/src/main/kotlin/net/turtton/ytalarm/ui/compose/theme/Color.kt
@@ -0,0 +1,9 @@
+package net.turtton.ytalarm.ui.compose.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Teal200 = Color(0xFF25E2C3)
+val Teal700 = Color(0xFF009666)
+val Red500 = Color(0xFFFF384D)
+val Red700 = Color(0xFFE22545)
+val Gray = Color(0xFFCCCCCC)
