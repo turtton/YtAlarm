d321c5f7b9a29087d41b41e9cfafa7ea011a2069
turtton <top.gear7509@turtton.net>
2025-12-30 01:18:51 +0900

    style: fix ktlint whitespace error in WorkInfo.kt

--- app/src/main/kotlin/net/turtton/ytalarm/util/extensions/WorkInfo.kt
+++ app/src/main/kotlin/net/turtton/ytalarm/util/extensions/WorkInfo.kt
@@ -2,9 +2,9 @@ package net.turtton.ytalarm.util.extensions
 
 import android.annotation.SuppressLint
 import androidx.work.WorkInfo
+import com.google.common.util.concurrent.ListenableFuture
 import kotlinx.coroutines.guava.await
 
 @SuppressLint("RestrictedApi")
-suspend fun com.google.common.util.concurrent.ListenableFuture<WorkInfo?>
-    .takeStateIfNotFinished(): WorkInfo.State? =
+suspend fun ListenableFuture<WorkInfo?>.takeStateIfNotFinished(): WorkInfo.State? =
     await()?.state?.takeUnless { it.isFinished }
