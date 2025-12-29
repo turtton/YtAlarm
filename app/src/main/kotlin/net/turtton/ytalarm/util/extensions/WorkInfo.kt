da55ca0d0b6660de1b03be51133a97c685f3402b
turtton <top.gear7509@turtton.net>
2025-12-30 01:00:18 +0900

    style: fix max line length in WorkInfo.kt

--- app/src/main/kotlin/net/turtton/ytalarm/util/extensions/WorkInfo.kt
+++ app/src/main/kotlin/net/turtton/ytalarm/util/extensions/WorkInfo.kt
@@ -5,5 +5,6 @@ import androidx.work.WorkInfo
 import kotlinx.coroutines.guava.await
 
 @SuppressLint("RestrictedApi")
-suspend fun com.google.common.util.concurrent.ListenableFuture<WorkInfo?>.takeStateIfNotFinished(): WorkInfo.State? =
+suspend fun com.google.common.util.concurrent.ListenableFuture<WorkInfo?>
+    .takeStateIfNotFinished(): WorkInfo.State? =
     await()?.state?.takeUnless { it.isFinished }
