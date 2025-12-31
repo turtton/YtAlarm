74751a80d784d368f16a8edb2a8a1b7ac3ccbca1
turtton <top.gear7509@turtton.net>
2025-12-31 17:16:02 +0900

    chore: Add imagemagick for mobile-mcp

--- flake.nix
+++ flake.nix
@@ -6,17 +6,25 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-   in with pkgs; rec {
-    devShells.default = mkShell {
-     packages = with pkgs; [
-        android-tools
-        fastlane
-     ];
-    };
-  });
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      rec {
+        formatter = pkgs.nixfmt-tree;
+        devShells.default = mkShell {
+          packages = with pkgs; [
+            android-tools
+            fastlane
+            imagemagick
+          ];
+        };
+      }
+    );
 }
